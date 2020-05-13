13e039d941675cbb926e3a00fb8d2de413b35f42
Justin Brooks <justin@datatorch.io>
2020-05-13 17:08:34 -0400

    fix: added region options to s3

--- packages/aws/src/AwsS3StorageOptions.ts
+++ packages/aws/src/AwsS3StorageOptions.ts
@@ -4,4 +4,7 @@ export interface AwsS3StorageOptions extends StorageOptions {
   bucket: string
   accessKeyId: string
   secretAccessKey: string
+  region?: string
+  endpoint?: string
+  useAccelerateEndpoint?: boolean
 }
